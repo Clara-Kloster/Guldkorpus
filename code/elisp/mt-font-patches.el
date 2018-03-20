80c3bdd58d76552649d368001de9d4957a8abc23
Ditte Christensen <dee.lfa@gmail.com>
2018-03-20 11:49:01 +0100

    corrected mrdes to is + added refcard to documentation

--- code/elisp/mt-font-patches.el
+++ code/elisp/mt-font-patches.el
@@ -3,6 +3,6 @@
 (set-fontset-font "fontset-default" '#xA75B "Andron Scriptor Web-13") ;; rrot
 (set-fontset-font "fontset-default" '#xA77A "Andron Scriptor Web-13") ;; drot
 (set-fontset-font "fontset-default" '#xF223 "Andron Scriptor Web-13") ;; mrdes
-(set-fontset-font "fontset-default" '#xA76D "Andron Scriptor Web-13") ;; mrdes
+(set-fontset-font "fontset-default" '#xA76D "Andron Scriptor Web-13") ;; is
 
 (provide 'mt-font-patches)
