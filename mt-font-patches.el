136fd50f1a228375dafd19839b756621e3059f70
Alex Speed Kjeldsen <ldz876@ad.sc.ku.dk>
2018-03-20 09:23:42 +0100

    Added mt-font-patches.el for Windows systems

---
+++ mt-font-patches.el
@@ -0,0 +1,8 @@
+;; Font-patches for Windows
+
+(set-fontset-font "fontset-default" '#xA75B "Andron Scriptor Web-13") ;; rrot
+(set-fontset-font "fontset-default" '#xA77A "Andron Scriptor Web-13") ;; drot
+(set-fontset-font "fontset-default" '#xF223 "Andron Scriptor Web-13") ;; mrdes
+(set-fontset-font "fontset-default" '#xA76D "Andron Scriptor Web-13") ;; mrdes
+
+(provide 'mt-font-patches)
