051e0be31ef48255b855e4bb465cc4a5ee4481c5
sdvrie <sean.vrieland@gmail.com>
2020-04-29 14:54:31 +0200

    Fixed POS-tags

--- code/python/find_problem_pos.py
+++ code/python/find_problem_pos.py
@@ -10,25 +10,57 @@ import sys
 sys.path.append(os.path.realpath('../..'))
 directory = sys.path[-1] + '/transcriptions/org/lemmatisation/'
 
-# Read user input for lemma and pos
-prob_lemma = sys.argv[1]
-prob_pos = sys.argv[2]
+print("This program searches for lemma-POS pairs")
 
-for filename in os.listdir(directory):
-    if filename.endswith(".org"):
-        problematic = False
-        number_of_problems = 0
-        working_file = directory + filename
-        shortname = filename.replace(".org", "")
-        data = open(working_file).read()
-        mo = re.search("Transcription\n.*\n(\n|$)", data, re.S)
-        mytable = mo.group(0)
-        d = csv.DictReader(mytable.splitlines(), delimiter = '|')
-        for row in d:
-            lemma = row[None][2].strip()
-            pos = row[None][3].strip()
-            if lemma == prob_lemma and pos == prob_pos:
-                problematic = True
-                number_of_problems += 1
-        if problematic == True:
-            print(shortname, " : ", number_of_problems)
+while True:
+    user_input = input("Please provide a lemma and POS-tag separated with a space: ")
+    user_input = user_input.split()
+
+    while len(user_input) != 2:
+        user_input = input("Oops try again: ")
+        user_input = user_input.split()
+
+    prob_lemma = user_input[0]
+    prob_pos = user_input[1]
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".org"):
+            problematic = False
+            number_of_problems = 0
+            working_file = directory + filename
+            shortname = filename.replace(".org", "")
+            data = open(working_file).read()
+            mo = re.search("Transcription\n.*\n(\n|$)", data, re.S)
+            mytable = mo.group(0)
+            d = csv.DictReader(mytable.splitlines(), delimiter = '|')
+            for row in d:
+                lemma = row[None][2].strip()
+                pos = row[None][3].strip()
+                if lemma == prob_lemma and pos == prob_pos:
+                    problematic = True
+                    number_of_problems += 1
+            if problematic == True:
+                print(shortname, " : ", number_of_problems)
+                    
+    continue_check = input("Press enter to continue or [q] to quit: ")
+    if continue_check == "q":
+        quit()
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".org"):
+            problematic = False
+            number_of_problems = 0
+            working_file = directory + filename
+            shortname = filename.replace(".org", "")
+            data = open(working_file).read()
+            mo = re.search("Transcription\n.*\n(\n|$)", data, re.S)
+            mytable = mo.group(0)
+            d = csv.DictReader(mytable.splitlines(), delimiter = '|')
+            for row in d:
+                lemma = row[None][2].strip()
+                pos = row[None][3].strip()
+                if lemma == prob_lemma and pos == prob_pos:
+                    problematic = True
+                    number_of_problems += 1
+            if problematic == True:
+                print("WARNING: ", shortname, " : ", number_of_problems)
