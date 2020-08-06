9bd9df112b35c1501f169aa6cb66aefcf8844928
Seán Vrieland <Sean@Seans-Office.local>
2020-08-06 13:05:47 +0200

    Updated files

--- code/python/Extract-Dipl.py
+++ code/python/Extract-Dipl.py
@@ -13,6 +13,8 @@ metafile = sys.path[-1] + '/documentation/corpus_lists.org'
 
 # Make list of Danish language files
 danish = [ ]
+danish_dates = { }
+danish_places = { }
 metadata = open(metafile).read()
 mo = re.search("NAME: Korpusliste\n.*\n(\n|$)", metadata, re.S)
 mytable = mo.group(0)
@@ -20,7 +22,10 @@ d = csv.DictReader(mytable.splitlines(), delimiter="|")
 for row in d:
     try:
         if row[None][8].strip() == "Dansk":
-            danish.append(row[None][0].strip())
+            charter = row[None][0].strip()
+            danish.append(charter)
+            danish_dates[charter] = row[None][5].strip()
+            danish_places[charter] = row[None][14].strip()
     except:
         pass
 danish.sort()
@@ -29,7 +34,7 @@ print("Number of files: " + str(len(danish)))
 # Read individual files
 write_file = open("/Users/Sean/Documents/Velux/Notes/AllDanish.org", "w+")
 for item in danish:
-    write_file.write("** " + item + "\n")
+    write_file.write("** " + item + " " + danish_places[item] + " " + danish_dates[item] + "\n")
     working_file = directory + item + '.org'
     data = open(working_file).read()
     mo = re.search("Transcription\n.*\n(\n|$)", data, re.S)
